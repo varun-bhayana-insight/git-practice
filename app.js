aa391c29ede1c7d8c71cc7a02766c7e426a0d5f8
varun-bhayana-insight <varun.bhayana@insight.com>
2025-12-30 17:07:14 +0530

    dev : add sub

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 console.log("Hello World");
 console.log("Welcome back");
 console.log("Welcome to dev branch")
+
+const a = 10;
+const b = 45;
+
+console.log(a);
+console.log(b);
+console.log(a+b);
+
+const c = 45;
+const d = 78;
+console.log(c-d);
+
